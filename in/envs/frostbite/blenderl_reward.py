8ea220456b7e409c683bfbca7ea8e0bd68b677ef
MaximilianHiebel <maximilian.hiebel@gmx.de>
2025-04-14 00:39:39 +0200

    changed reward

--- in/envs/frostbite/blenderl_reward.py
+++ in/envs/frostbite/blenderl_reward.py
@@ -5,16 +5,15 @@ def reward_function(self) -> float:
     for obj in self.objects:
         if 'player' in str(obj).lower():
             player = obj
-            break
+        if 'door' in str(obj).lower():
+            door_exists = True
 
-    if self.org_reward == 1.0 and player.y != 45:
-        reward = 1.0
-    else:
-        reward = 0.0
-    #great reward for completing stage
-    #small reward for collecting fish
-    #medium reward for high remaining temperature
-    #tiny reward for collecting iceblock
-    #small penalty for switching direction?
-    #great penalty for dying
+    reward = 0.0
+
+    #surviving
+    reward += 0.05
+
+    #door_exists
+    reward += 0.5
+    
     return reward
