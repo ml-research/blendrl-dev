7824cb2a77c9a433ba8e996391124ab3725a8997
Henri Rößler <henri.roessler@googlemail.com>
2025-05-05 17:22:41 +0200

    added heatmap overlay to renderer for given predicate and valuation model

--- play_gui.py
+++ play_gui.py
@@ -1,16 +1,27 @@
-import tyro
-from blendrl.renderer import Renderer
+from typing import Optional
 
-from dataclasses import dataclass
+import torch
 import tyro
+
+from blendrl.renderer import Renderer
+from valuation.utils import ValuationExperiment
 
 
 def main(
-    env_name: str = "seaquest",
+    env_name: str = "kangaroo",
     agent_path: str = "models/kangaroo_demo",
+    exp_name: str = "",
     seed: int = 0,
     fps: int = 5,
+    predicate_name: Optional[str] = None,
 ) -> None:
+
+    # load predicate model
+    experiment = ValuationExperiment.from_name(exp_name)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    valuation_model = experiment.get_valuation_model(device)
+
+    # create renderer
     renderer = Renderer(
         agent_path=agent_path,
         env_name=env_name,
@@ -19,7 +30,11 @@ def main(
         env_kwargs=dict(render_oc_overlay=True),
         render_predicate_probs=True,
         seed=seed,
+        predicate_name=predicate_name,
+        valuation_model=valuation_model,
     )
+
+    # run renderer
     renderer.run()
 
 
