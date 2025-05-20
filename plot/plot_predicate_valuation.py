9b552d1e57f96cbf4d23e499e3c861b9c0e56eb7
Henri Rößler <henri.roessler@googlemail.com>
2025-05-20 23:09:43 +0200

    extended to plot multiple activation maps at once, including for specific checkpoints

--- plot/plot_predicate_valuation.py
+++ plot/plot_predicate_valuation.py
@@ -2,91 +2,121 @@ import os
 from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
-import torch
 import torch as th
 from matplotlib import animation
 from tqdm import tqdm
 
-from utils import load_model_state
+from utils import load_model_state, get_default_device, FRAME_SIZE
 from valuation.utils import ValuationExperiment
+
+
+CMAP = 'jet'
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--exp-name", type=str, required=True)
-    parser.add_argument("--predicate-name", type=str, required=True)
-    parser.add_argument("--position", type=int, default=5)
+
+    parser.add_argument("--exp-name", nargs='+', type=str, required=True)
+    parser.add_argument("--predicate-name", nargs='+', type=str, required=True)
+    parser.add_argument("--position", nargs='+', type=int, default=5)
+    parser.add_argument("--checkpoint", nargs='+', type=int, default=None)
+
     parser.add_argument("--interval", type=int, default=40)
     parser.add_argument("--skip", type=int, default=0)
+    parser.add_argument("--cell-size", type=int, default=5)
+
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    device = torch.device('cpu')
-    experiment = ValuationExperiment.from_name(args.exp_name)
+    device = get_default_device()
 
-    predicate_model = experiment.get_valuation_model(device, load_from_latest_checkpoint=False)
-    plots_dir = experiment.plots_dir
+    for exp_name in args.exp_name:
+        # Load experiment
+        experiment = ValuationExperiment.from_name(exp_name)
 
-    os.makedirs(plots_dir, exist_ok=True)
+        # Load valuation model
+        valuation_model = experiment.get_valuation_model(device, load_from_latest_checkpoint=False)
 
-    frame_size = (160, 210)
-    frame_grid = (16, 21)
-    num_grid_cells = frame_grid[0] * frame_grid[1]
-    grid_cell_size = (frame_size[0] / frame_grid[0], frame_size[1] / frame_grid[1])
+        checkpoints = experiment.checkpoints
+        if len(checkpoints) == 0:
+            print("No checkpoints found")
+            continue
+        checkpoint_steps = [checkpoints[-1].step] if args.checkpoint is None else args.checkpoint
 
-    x_mul = 0.25 * ((args.position + 2) % 3 + 1)
-    y_mul = 0.25 * ((args.position + 2) // 3)
-    obj_pos = (int(frame_size[0] * x_mul), int(frame_size[1] * y_mul))
-    obj_grid_pos = (obj_pos[0] / frame_size[0] * frame_grid[0], obj_pos[1] / frame_size[1] * frame_grid[1])
-    obj_input = th.tensor([1, obj_pos[0], obj_pos[1], 0]).repeat(num_grid_cells, 1)
+        plots_dir = experiment.plots_dir / "predicate_valuation"
+        os.makedirs(plots_dir, exist_ok=True)
 
-    player_input = th.tensor([1, 0, 0, 0]).repeat(num_grid_cells, 1)
-    idx = 0
-    for i in range(frame_grid[0]):
-        x = (frame_size[0] / frame_grid[0]) * (i + 0.5)
-        for j in range(frame_grid[1]):
-            y = (frame_size[1] / frame_grid[1]) * (j + 0.5)
-            player_input[idx, 1] = x
-            player_input[idx, 2] = y
-            idx += 1
+        # Calculate grid
+        frame_size = FRAME_SIZE[experiment.env_name]
+        frame_grid = (int(frame_size[0] // args.cell_size), int(frame_size[1] // args.cell_size))
+        num_grid_cells = frame_grid[0] * frame_grid[1]
 
-    checkpoints = experiment.checkpoints[args.skip::args.skip+1]
-    num_checkpoints = len(checkpoints)
+        # Calculate player positions
+        player_input = th.tensor([1, 0, 0, 0], device=device).repeat(num_grid_cells, 1)
+        idx = 0
+        for i in range(frame_grid[0]):
+            x = (frame_size[0] / frame_grid[0]) * (i + 0.5)
+            for j in range(frame_grid[1]):
+                y = (frame_size[1] / frame_grid[1]) * (j + 0.5)
+                player_input[idx, 1] = x
+                player_input[idx, 2] = y
+                idx += 1
 
-    if len(checkpoints) == 0:
-        print("No checkpoints found")
-        return
+        for position in args.position:
+            x_mul = 0.25 * ((position + 2) % 3 + 1)
+            y_mul = 0.25 * ((position + 2) // 3)
+            obj_pos = (int(frame_size[0] * x_mul), int(frame_size[1] * y_mul))
+            obj_grid_pos = (obj_pos[0] / frame_size[0] * frame_grid[0], obj_pos[1] / frame_size[1] * frame_grid[1])
+            obj_input = th.tensor([1, obj_pos[0], obj_pos[1], 0], device=device).repeat(num_grid_cells, 1)
 
-    load_model_state(checkpoints[0].path, predicate_model)
+            for predicate_name in args.predicate_name:
+                for checkpoint in checkpoints:
+                    if checkpoint.step in checkpoint_steps:
+                        load_model_state(checkpoint.path, valuation_model)
 
-    fig, ax = plt.subplots()
-    output = predicate_model(args.predicate_name, player_input, obj_input).view(*frame_grid).detach().numpy()
-    im = ax.imshow(output.T, cmap='Reds', vmin=0, vmax=1)
-    ax.scatter(*obj_grid_pos, color='black')
-    title = ax.set_title(f"Checkpoint step {checkpoints[0].step}")
+                        fig, ax = plt.subplots()
+                        output = valuation_model(predicate_name, player_input, obj_input).view(*frame_grid).detach().cpu().numpy()
+                        im = ax.imshow(output.T, cmap=CMAP, vmin=0, vmax=1)
+                        ax.scatter(*obj_grid_pos, color='white')
+                        ax.set_xticks([])
+                        ax.set_yticks([])
+                        fig.suptitle(predicate_name, fontsize=10, y=0.95)
+                        subtitle = ax.set_title(f"{exp_name} (step {checkpoint.step})", fontsize=8)
+                        fig.tight_layout()
 
-    out_path = plots_dir / f"activation_{args.predicate_name}_{args.position}.gif"
+                        still_filename = f"{predicate_name}_{position}"
+                        if checkpoint.step != checkpoints[-1].step:
+                            still_filename += f"_{checkpoint.step}"
+                        still_out_path = plots_dir / f"{still_filename}.png"
+                        fig.savefig(still_out_path, dpi=150, bbox_inches='tight')
+                        print(f"Checkpoint {checkpoint.step} saved at {still_out_path}")
 
-    with tqdm(desc="Animating frame", total=num_checkpoints) as pbar:
-        def update(frame):
-            pbar.update()
-            checkpoint = checkpoints[frame]
-            load_model_state(checkpoint.path, predicate_model)
-            output = predicate_model(args.predicate_name, player_input, obj_input).view(*frame_grid).detach().numpy()
-            im.set_array(output.T)
-            title.set_text(f"Checkpoint step {checkpoint.step}")
-            return [im, title]
+                if args.checkpoint is None:
+                    ani_out_path = plots_dir / f"{predicate_name}_{position}.gif"
 
-        ani = animation.FuncAnimation(fig, update, frames=num_checkpoints, blit=True, interval=args.interval)
+                    _checkpoints = checkpoints[::args.skip]
+                    num_checkpoints = len(_checkpoints)
 
-        ani.save(out_path, writer='pillow')
+                    with tqdm(desc=f"{exp_name} - Animating {predicate_name} {position}", total=num_checkpoints) as pbar:
+                        def update(frame):
+                            pbar.update()
+                            checkpoint = _checkpoints[frame]
+                            load_model_state(checkpoint.path, valuation_model)
+                            output = valuation_model(predicate_name, player_input, obj_input).view(*frame_grid).detach().cpu().numpy()
+                            im.set_array(output.T)
+                            subtitle.set_text(f"{exp_name} (step {checkpoint.step})")
+                            return [im, subtitle]
 
-    plt.close(fig)
+                        ani = animation.FuncAnimation(fig, update, frames=num_checkpoints, blit=True, interval=args.interval)
 
-    print(f"Animation saved at {out_path}")
+                        ani.save(ani_out_path, writer='pillow')
+
+                    print(f"Animation saved at {ani_out_path}")
+                    plt.close(fig)
+
 
 
 if __name__ == '__main__':
